25442731dfa75f67bb80fe4493aea3f44044afbd
Chris Dostert <chris.dostert@live.com>
2015-04-05 11:33:42 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+![](https://ci.appveyor.com/api/projects/status/rayv6xsibmqf48e8?svg=true)
+
+####What is it?
+
+An [Appease](http://appease.io) task template that sets an [Azure App Service plan](http://azure.microsoft.com/en-us/documentation/articles/azure-web-sites-web-hosting-plans-in-depth-overview/).
+
+####How do I install it?
+
+```PowerShell
+Add-AppeaseTask `
+    -DevOpName YOUR-DEVOP-NAME `
+    -TemplateId SetAzureAppServicePlan
+```
+
+####What parameters are required?
+
+#####Name
+description: a `string` representing the name of the App Service plan.
+
+#####ResourceGroupName
+description: a `string` representing the name of the resource group this App Service plan will be added to.
+
+#####Location
+description: a `string` representing the geographical location of the resource group.
+allowed values: 
+```PowerShell
+AzureResourceManager\Get-AzureLocation | ?{$_.Name -eq 'Microsoft.Web/serverFarms'} | select LocationsString
+
+LocationsString                                                                                                                                                                             
+---------------                                                                                                                                                                             
+Brazil South, East Asia, East US, Japan East, Japan West, North Central US, North Europe, South Central US, West Europe, West US, Southeast Asia, Central US, East US 2
+
+```
+
+#####Sku
+description: a `string` representing the sku of the App Service plan 
+allowed values: `Free`, `Shared`, `Basic`, `Standard`, `Premium`
+
+#####WorkerSize
+description: a `string` representing the size of each worker VM allocated to the App Service plan
+
+#####NumberOfWorkers
+description: an `int` representing the number of worker VMs allocated to the App Service plan
+
+####What parameters are optional?
+
+#####Tags
+description: a `PSCustomObject[]` representing tags (labels) to associate with the App Service plan
+schema:
+```PowerShell
+@(
+    [PSCustomObject]@{
+        'YOUR-TAG-KEY'='YOUR-TAG-VALUE'
+    }
+)
+```
---
+++ Src/SetAzureAppServicePlan.psm1
@@ -0,0 +1,90 @@
+# halt immediately on any errors which occur in this module
+$ErrorActionPreference = 'Stop'
+
+function Invoke(
+
+    [string]
+    [ValidateNotNullOrEmpty()]
+    [Parameter(
+        Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true)]
+    $Name,
+
+    [string]
+    [ValidateNotNullOrEmpty()]
+    [Parameter(
+        Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true)]
+    $ResourceGroupName,
+
+    [string]
+    [ValidateNotNullOrEmpty()]
+    [Parameter(
+        Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true)]
+    $Location,
+
+    [string]
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet(Free, Shared, Basic, Standard, Premium)]
+    [Parameter(
+        Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true)]
+    $Sku,
+
+    [string]
+    [ValidateNotNullOrEmpty()]
+    [Parameter(
+        Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true)]
+    $WorkerSize,
+
+    [string]
+    [ValidateNotNullOrEmpty()]
+    [Parameter(
+        Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true)]
+    $NumberOfWorkers,
+
+    [PSCustomObject]
+    [Parameter(
+        Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true)]
+    $Tag
+){
+
+    $ApiVersion = '2014-04-01'
+    $ResourceType = 'Microsoft.Web/serverFarms'
+
+    # build up property Hashtable from parameters
+    $Properties = @{'sku'=$Sku;'workerSize'=$WorkerSize;'numberOfWorkers'=$NumberOfWorkers}
+
+    # build up tag Hashtable from tag PSCustomObject
+    $TagHashtable = @{}
+    if($Tag){
+        $Tag.PSObject.Properties | %{$TagHashtable[$_.Name]=$_.Value}
+    }
+
+    If(!(Get-AzureResource | ?{($_.Name -eq $Name) -and ($_.ResourceGroupName -eq $ResourceGroupName) -and ($_.Location -eq $Location)})){
+        New-AzureResource `
+        -Location $Location `
+        -Name $Name `
+        -ResourceGroupName $ResourceGroupName `
+        -ResourceType $ResourceType `
+        -Tag $TagHashtable `
+        -ApiVersion $ApiVersion `
+        -PropertyObject $Properties `
+        -Force
+    }
+    Else{
+        Set-AzureResource `
+        -Name $Name `
+        -ResourceGroupName $ResourceGroupName `
+        -ResourceType $ResourceType `
+        -Tag $TagHashtable `
+        -ApiVersion $ApiVersion `
+        -PropertyObject $Properties
+    }
+}
+
+Export-ModuleMember -Function Invoke
